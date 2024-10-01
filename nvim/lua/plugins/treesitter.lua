48da09618691f99319d3e7e4d5cf2665daeb6f24
John Benson <jonebens@amazon.com>
2024-10-01 13:33:14 +0000

    Fix treesiter and add lualine

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+return {
+  'nvim-treesitter/nvim-treesitter',
+  opts = function(_, opts) 
+    opts.ignore_install = { 'help' }
+    if type(os.ensure_installed) == 'table' then
+      vim.list_extend(opts.ensure_installed, {
+        'dockerfile',
+        'git_config',
+        'jsdoc',
+        'make',
+        'toml',
+        'vimdoc'
+      })
+    end
+  end,
+}
